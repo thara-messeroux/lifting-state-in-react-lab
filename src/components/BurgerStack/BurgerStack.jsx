fd1ac70002a0f08a5cb4416f1f3fb46436f4fd20
thara-messeroux <tharamesseroux@gmail.com>
2026-04-09 06:43:18 -0400

    feat: complete GA setup with starter app structure and components

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,11 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+// This component will be responsible for rendering
+// the stack of ingredients that the user has chosen
+// It will receive the list of ingredients as props from App.jsx
+
+const BurgerStack = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default BurgerStack;
---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,11 @@
+// src/components/IngredientList/IngredientList.jsx
+
+// This component will be responsible for rendering 
+// the list of available ingredients
+// It will receive the list of ingredients as props from App.jsx
+
+const IngredientList = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default IngredientList;
